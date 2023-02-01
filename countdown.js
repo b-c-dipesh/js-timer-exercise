ce1684fd19adf2236e09abf9a4a1224a951dc835
bc-dipesh <bcdipeshwork@gmail.com>
2023-02-01 18:40:37 -0500

    Create UI for countdown and random game

--- countdown.js
+++ countdown.js
@@ -1,17 +1,54 @@
 'use strict';
 
+// select DOM elements
+const countDownForm = document.querySelector('.countdown-form');
+const countDownInput = document.querySelector('#countdown-input');
+const countDownStartBtn = document.querySelector('.countdown-start-btn');
+const countDownResults = document.querySelector('.countdown-results');
+
+// display results in html
+const printInScreen = (result) => {
+	// create a li element
+	const li = document.createElement('li');
+
+	// add result inside the li element
+	li.innerText = result;
+
+	// append it to the results
+	countDownResults.append(li);
+};
+
+// countDown logic
 const countDown = (counter) => {
 	const INTERVAL_ID = setInterval(() => {
 		--counter;
 
 		if (counter === 0) {
-			console.log('DONE!');
+			printInScreen('DONE!');
 			clearInterval(INTERVAL_ID);
+			countDownInput.disabled = false;
+			countDownStartBtn.disabled = false;
 			return;
 		}
 
-		console.log(counter);
+		printInScreen(counter);
 	}, 1000);
 };
 
-countDown(20);
+// intercept the form submission
+countDownForm.addEventListener('submit', (e) => {
+	e.preventDefault();
+
+	// clear previous countdown results if any
+	countDownResults.innerHTML = '';
+
+	// extract value from form input and parse it to int
+	const countDownStartVal = parseInt(countDownInput.value);
+
+	// disable the input field and button until the countdown is finished
+	countDownInput.disabled = true;
+	countDownStartBtn.disabled = true;
+
+	// start countDown
+	countDown(countDownStartVal);
+});
--- random-game.js
+++ random-game.js
@@ -1,18 +1,50 @@
 'use-strict';
 
+// select DOM elements
+const randomGameStartBtn = document.querySelector('.random-game-start-btn');
+const randomGameResults = document.querySelector('.random-game-results');
+
+// display results in html
+const displayGameResults = (randomNumber, count = '') => {
+	// create a li element
+	const li = document.createElement('li');
+
+	// add result inside li element
+	if (count) {
+		li.innerText = `Current random number is --> ${randomNumber} and number of tries is --> ${count}`;
+	} else {
+		li.innerText = `Current random number is --> ${randomNumber}`;
+	}
+
+	// append li to results
+	randomGameResults.append(li);
+};
+
+// randomGame logic
 const randomGame = () => {
 	let randomNumber;
 	let counter = 0;
 	const INTERVAL_ID = setInterval(() => {
 		randomNumber = Math.random();
 		++counter;
-		console.log(`Current random number is --> ${randomNumber}`);
+		displayGameResults(randomNumber);
 
 		if (randomNumber > 0.75) {
-			console.log(counter);
+			displayGameResults(randomNumber, counter);
 			clearInterval(INTERVAL_ID);
+			randomGameStartBtn.disabled = false;
 		}
 	}, 1000);
 };
 
-randomGame();
+// add Event listener
+randomGameStartBtn.addEventListener('click', (e) => {
+	// clear any previous randomGame results if any
+	randomGameResults.innerHTML = '';
+
+	// disable the start game button until the game is finished
+	randomGameStartBtn.disabled = true;
+
+	// start randomGame
+	randomGame();
+});
